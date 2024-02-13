b61a7465ef887e030d9172dcb01b22f5ba714c00
Joaquin Agras <joaquinagras@gmail.com>
2024-02-13 17:30:28 +0100

    validacion usuario

--- scriptApp.js
+++ scriptApp.js
@@ -1,16 +1,49 @@
-// asignaciones variables a elementos DOM
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-
-// variables para verificar usuarios en BBDD
-const usernameBBDD = "";
-const passwordBBDD = "";
-
 // Login
 function validateLogin() {
-  if (username.value === usernameBBDD && password.value === passwordBBDD) {
-    document.querySelector(".global-container").style.opacity = 0;
-  } else {
-    alert("Usuario o contraseña incorrectos");
+  // asigno variables que recogo de formulario para validacion
+  let usernameValue = document.getElementById("username").value;
+  let passwordValue = document.getElementById("password").value;
+  let mensajeError = document.getElementById("errorUsuario");
+
+  // Validaciones user y pwd
+  if (!usernameValue || !passwordValue) {
+    mensajeError.textContent = "Introduce usuario y password";
+    // alert("Introduce usuario y password");
+
+    return;
+  } else if (passwordValue.length < 4) {
+    // alert("Password menor de 4 caracteres");
+    mensajeError.textContent = "Password menor de 4 caracteres";
+    return;
   }
+
+  // creo objeto que almacena user y pwd
+  const datosUsuario = {
+    username: usernameValue,
+    password: passwordValue,
+  };
+
+  // convertir a json
+  let jsonUsuario = JSON.stringify(datosUsuario);
+  console.log(jsonUsuario);
+
+  // configurar peticion ajax a mysql
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/api/login", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // funcion para manejar la respuesta del servidor
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      setUserCookie(usernameValue);
+      // quitar la opacidad de la parte de abajo de la aplicacion
+      window.localtion.href = "/secondScreen.html"; // pdte cambiar
+      // el .global-container donde pondremos pintaremos la aplicacion
+    } else {
+      // Si la respuesta no es exitosa mostrar error
+      // alert("Error al iniciar sesión" + xhr.responseText);
+      mensajeError.textContent = "Error al iniciar sesión" + xhr.responseText;
+    }
+  };
+  xhr.send(jsonUsuario);
 }
